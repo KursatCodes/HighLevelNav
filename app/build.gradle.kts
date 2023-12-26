6bd24e60be62ee88fa73cc34e3530493f3a3565a
Muhammed <kgokgun94@gmail.com>
2023-12-26 07:00:28 +0300

    Back button added, arguments added.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,15 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("androidx.navigation.safeargs")
 }
 
 android {
+    dataBinding {
+        enable = true
+    }
     namespace = "com.muhammedkursat.highlevelnavigation"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.muhammedkursat.highlevelnavigation"
@@ -37,12 +41,13 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
+    implementation("androidx.fragment:fragment:1.6.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/muhammedkursat/highlevelnavigation/MainActivity.kt
+++ app/src/main/java/com/muhammedkursat/highlevelnavigation/MainActivity.kt
@@ -2,10 +2,25 @@ package com.muhammedkursat.highlevelnavigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 class MainActivity : AppCompatActivity() {
+    private lateinit var navigationController : NavController
+//    val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+//    var navController = navHostFragment.navController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+    navigationController = navHostFragment.navController
+    //navigationController = Navigation.findNavController(this,R.id.fragmentContainerView)
+    NavigationUI.setupActionBarWithNavController(this,navigationController)
+
     }
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navigationController,null)
+    }
+
 }
